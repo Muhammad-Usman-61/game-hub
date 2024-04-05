13a6d97671f1a0fb6bb9c12f5880cedb1daff12e
Muhammad-Usman-61 <muhammad196usman@gmail.com>
2024-04-06 00:09:09 +0500

    grid area fixed (position of aside)

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,14 @@ function App() {
     <Grid
       templateAreas={{
         base: `"nav" "main"`,
-        lg: `"nav nav" "main aside"`,
+        lg: `"nav nav" "aside main"`,
       }}
     >
       <GridItem area="nav">
         <NavBar />
       </GridItem>
       <Show above="lg">
-        <GridItem area="aside" bg="green">
+        <GridItem area="aside" bg="lightgreen">
           aside
         </GridItem>
       </Show>
