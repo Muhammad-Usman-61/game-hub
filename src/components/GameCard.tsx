ea6232331dd2476a2d482fefd8b9a61dff042ccc
Muhammad-Usman-61 <muhammad196usman@gmail.com>
2024-04-09 05:31:21 +0500

    font size for game name changed

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -10,7 +10,7 @@ const GameCard = ({ game }: Props) => {
     <Card borderRadius={10} overflow="hidden">
       <Image src={game.background_image}></Image>
       <CardBody>
-        <Heading fontSize="2xl">{game.name}</Heading>
+        <Heading fontSize="xl">{game.name}</Heading>
       </CardBody>
     </Card>
   );
