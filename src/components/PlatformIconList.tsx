ca448cc40fd5e22ff0176cefaa9c96b55f01d093
Muhammad-Usman-61 <muhammad196usman@gmail.com>
2024-04-10 23:30:23 +0500

    platform icons added to game card

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -1,17 +1,39 @@
 import { Platform } from "../hooks/useGames";
-import { Text } from "@chakra-ui/react";
-
+import { HStack, Icon } from "@chakra-ui/react";
+import {
+  FaWindows,
+  FaXbox,
+  FaPlaystation,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import { IconType } from "react-icons";
 interface Props {
   platforms: Platform[];
 }
 
 const PlatformIconList = ({ platforms }: Props) => {
+  const iconMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    xbox: FaXbox,
+    playstation: FaPlaystation,
+    mac: FaApple,
+    linux: FaLinux,
+    android: FaAndroid,
+    web: BsGlobe,
+    ios: MdPhoneIphone,
+    nintendo: SiNintendo,
+  };
   return (
-    <>
+    <HStack marginY={1}>
       {platforms.map((platform) => (
-        <Text key={platform.id}>{platform.name}</Text>
+        <Icon as={iconMap[platform.slug]} color="gray.500" />
       ))}
-    </>
+    </HStack>
   );
 };
 
