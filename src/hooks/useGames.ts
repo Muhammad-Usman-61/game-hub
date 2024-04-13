4c9d2c9c1a69fdb7dcd3ac5b3bbeedc0215ebef5
Muhammad-Usman-61 <muhammad196usman@gmail.com>
2024-04-13 23:42:43 +0500

    game card skeletons added

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -23,6 +23,7 @@ interface FetchGameResponse {
 const useGames = () => {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(true);
 
   useEffect(() => {
     const controller = new AbortController();
@@ -30,16 +31,18 @@ const useGames = () => {
       .get<FetchGameResponse>("/games", { signal: controller.signal })
       .then((res) => {
         setGames(res.data.results);
+        setLoading(false);
       })
       .catch((err) => {
         if (err instanceof CanceledError) return;
         setError(err.message);
+        setLoading(false);
       });
 
     return () => controller.abort();
   }, []);
 
-  return { games, error };
+  return { games, error, isLoading };
 };
 
 export default useGames;
