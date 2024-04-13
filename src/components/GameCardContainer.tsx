1f4f1d7f6bd0480b69361560c39406dce274910e
Muhammad-Usman-61 <muhammad196usman@gmail.com>
2024-04-14 00:18:02 +0500

    gamegrid styling fixed

--- src/components/GameCardContainer.tsx
+++ src/components/GameCardContainer.tsx
@@ -5,7 +5,7 @@ interface Props {
 }
 const GameCardContainer = ({ children }: Props) => {
   return (
-    <Box width="210px" borderRadius={10} overflow="hidden">
+    <Box width="250px" borderRadius={10} overflow="hidden">
       {children}
     </Box>
   );
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,4 +1,4 @@
-import { SimpleGrid, Text } from "@chakra-ui/react";
+import { Center, SimpleGrid, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
@@ -6,13 +6,14 @@ import GameCardContainer from "./GameCardContainer";
 
 const GameGrid = () => {
   const { games, error, isLoading } = useGames();
-  const skeletons = [1, , 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const skeletons = [1, , 2, 3, 4, 5, 6, 7, 8];
   return (
     <>
       {error && <Text>{error}</Text>}
       <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
-        spacing={10}
+        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+        spacingY={10}
+        justifyContent="Center"
         padding="10px"
       >
         {isLoading &&
