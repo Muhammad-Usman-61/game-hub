aa2248c5201e93ad5e992abb6cf3245535fa4dc1
Muhammad-Usman-61 <muhammad196usman@gmail.com>
2024-04-21 21:24:47 +0500

    added filter to platform selector

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -1,8 +1,12 @@
 import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
-import usePlatforms from "../hooks/usePlatforms";
+import usePlatforms, { Platforms } from "../hooks/usePlatforms";
 
-const PlatformSelector = () => {
+interface Props {
+  onSelectPlatform: (platform: Platforms) => void;
+}
+
+const PlatformSelector = ({ onSelectPlatform }: Props) => {
   const { data, error } = usePlatforms();
 
   if (error) return null;
@@ -13,8 +17,15 @@ const PlatformSelector = () => {
         Platforms
       </MenuButton>
       <MenuList>
-        {data.map((platfrom) => (
-          <MenuItem key={platfrom.id}>{platfrom.name}</MenuItem>
+        {data.map((platform) => (
+          <MenuItem
+            onClick={() => {
+              onSelectPlatform(platform);
+            }}
+            key={platform.id}
+          >
+            {platform.name}
+          </MenuItem>
         ))}
       </MenuList>
     </Menu>
